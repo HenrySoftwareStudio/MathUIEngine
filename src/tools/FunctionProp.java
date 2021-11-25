e9f055050ea8d50dfaed1bd420acb1f459cbd127
Henry F <henryfeng.developer@gmail.com>
2021-11-25 10:07:01 -0500

    Sync-10:06 11/25/2021

--- src/tools/FunctionProp.java
+++ src/tools/FunctionProp.java
@@ -9,18 +9,20 @@ public class FunctionProp implements Serializable
 	 */
 	private static final long serialVersionUID = -2515774817797271903L;
 	private final String name;
-	private final String[] launchArg;
+	private final String launchArg;
 	private final String toolTipMessage;
 	private final int LookupKey;
-	public FunctionProp(final String Name, final String[] LaunchArg, final String TooltipMessage)
+	private final int inputFieldCount;
+	public FunctionProp(final String Name, final String LaunchArg, final String TooltipMessage, final int InputFieldCount)
 	{
-		name=Name;
-		launchArg=LaunchArg;
-		toolTipMessage=TooltipMessage;
-		LookupKey=hashCode();
+		name = Name;
+		launchArg = LaunchArg;
+		toolTipMessage = TooltipMessage;
+		inputFieldCount = InputFieldCount;
+		LookupKey = hashCode();
 	}
 	
-	public String[] getLaunchArg()
+	public String getLaunchArg()
 	{
 		return launchArg;
 	}
@@ -38,5 +40,10 @@ public class FunctionProp implements Serializable
 	public int getLookupKey()
 	{
 		return LookupKey;
+	}
+	
+	public int getInputFieldCount()
+	{
+		return inputFieldCount;
 	}
 }
--- src/tools/ReadingTool.java
+++ src/tools/ReadingTool.java
@@ -1,20 +1,54 @@
 package tools;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class ReadingTool
 {
 	private final File workingOn;
 	public ReadingTool(final File filePath)
 	{
-		workingOn=filePath;
-		System.out.println(workingOn);
+		workingOn = filePath;
 	}
 	
-	public ArrayList<FunctionProp> read()
+	public ArrayList<FunctionProp> read() throws ParserConfigurationException, SAXException, IOException
 	{
-		return null; //TODO: work on this
+		ArrayList<FunctionProp> ret = new ArrayList<>(1);
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(workingOn);
+        doc.getDocumentElement().normalize();
+	    dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+        NodeList MainNodeList = doc.getElementsByTagName("function");
+        System.out.println(MainNodeList.item(0));
+        for (int temp = 0; temp < MainNodeList.getLength(); temp++) 
+        {
+        	Node node = MainNodeList.item(temp);
+        	if (node.getNodeType() == Node.ELEMENT_NODE) 
+	        {
+        		Element idvFunction = (Element) node;
+        		System.out.print(node);
+        		String name = idvFunction.getAttribute("name");
+        		String launchArgs = idvFunction.getAttribute("launchArgs");
+				String tooltip = idvFunction.getAttribute("tooltip");
+				int valueCount = Integer.parseInt(idvFunction.getAttribute("valuesCount"));
+				System.out.print("Here");
+				ret.add(new FunctionProp(name, launchArgs, tooltip, valueCount));    		
+	        }
+        }
+		return ret;
 	}
 	
 	public File getWorkingOn()
